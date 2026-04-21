68e3a287605d2494f5f111d309633022a6196d0d
Eisha44 <muzaffareisha@gmail.com>
2026-04-20 21:16:24 -0700

    Lab 5 added

---
+++ Lab_6/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  // Function to play the sound
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play('assets/note$soundNumber.wav');
+  }
+
+  // Function to build a key
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Text('Xylophone', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.yellow, soundNumber: 2),
+              buildKey(color: Colors.green, soundNumber: 3),
+              buildKey(color: Colors.lime, soundNumber: 4),
+              buildKey(color: Colors.brown, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
